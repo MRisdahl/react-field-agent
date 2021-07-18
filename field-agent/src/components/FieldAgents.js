604b0f029c24cb95b03d9b84cb4368de1aa6ccf8
Marc Risdahl <MRisdahl@dev-10.com>
2021-07-18 16:33:03 -0500

    Finish agent list

--- field-agent/src/components/FieldAgents.js
+++ field-agent/src/components/FieldAgents.js
@@ -88,14 +88,14 @@ function FieldAgents() {
                         </nav>
                         <main>
                             <div className="cards">
-
-                                <div className="card">
+                                {agents.map(agent => (
+                                    <div className="card">
                                     <header>
-                                        <h3>Hazel C. Sauven</h3>
+                                        <h3>{agent.firstName} {agent.middleName} {agent.lastName}</h3>
                                     </header>
                                     <ul>
-                                        <li><span>DOB:</span> 1954-09-16</li>
-                                        <li><span>Height in Inches:</span> 76</li>
+                                        <li><span>DOB:</span> {agent.dob}</li>
+                                        <li><span>Height in Inches:</span> {agent.heightInInches}</li>
                                     </ul>
                                     <footer>
                                         <div>
@@ -105,126 +105,7 @@ function FieldAgents() {
                                         </div>
                                     </footer>
                                 </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Claudian C. O'Lynn</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> 1956-11-09</li>
-                                        <li><span>Height in Inches:</span> 41</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Winn V. Puckrin</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> 1999-10-21</li>
-                                        <li><span>Height in Inches:</span> 60</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Kiab U. Whitham</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> 1960-07-29</li>
-                                        <li><span>Height in Inches:</span> 52</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Min E. Dearle</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> 1967-04-18</li>
-                                        <li><span>Height in Inches:</span> 44</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Urban H. Carwithen</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> N/A</li>
-                                        <li><span>Height in Inches:</span> 58</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Ulises B. Muhammad</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> 2008-04-01</li>
-                                        <li><span>Height in Inches:</span> 80</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
-                                <div className="card">
-                                    <header>
-                                        <h3>Phylys Y. Howitt</h3>
-                                    </header>
-                                    <ul>
-                                        <li><span>DOB:</span> 1979-03-28</li>
-                                        <li><span>Height in Inches:</span> 68</li>
-                                    </ul>
-                                    <footer>
-                                        <div>
-                                            <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
-                                            &nbsp;|&nbsp;
-                                            <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
-                                        </div>
-                                    </footer>
-                                </div>
-
+                                ))}
                             </div>
                         </main>
                         <footer>
