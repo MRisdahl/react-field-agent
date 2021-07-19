50d64a052a6da595a1f20e74ca799199f915c01b
Marc Risdahl <MRisdahl@dev-10.com>
2021-07-18 19:58:38 -0500

    Add edit and delete feature

--- field-agent/src/components/FieldAgents.js
+++ field-agent/src/components/FieldAgents.js
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import data from "../data";
 
 function FieldAgents() {
-    const [currentView, setCurrentView] = useState('Add');
+    const [currentView, setCurrentView] = useState('Home');
     const [firstName, setFirstName] = useState('');
     const [middleName, setMiddleName] = useState('');
     const [lastName, setLastName] = useState('');
     const [dob, setDob] = useState('');
-    const []
+    const [editAgentId, setEditAgentId] = useState(0);
     const [heightInInches, setHeightInInches] = useState('');
     const [agents, setAgents] = useState(data);
 
@@ -69,11 +69,50 @@ function FieldAgents() {
         event.preventDefault();
 
         const newAgent = {
+            id: editAgentId,
+            firstName,
+            middleName,
+            lastName,
+            dob,
+            heightInInches,
+            aliases: [],
+            agencies: []
+        };
 
-        }
+        const newAgents = [...agents];
+
+        const newAgentIndex = newAgents.findIndex(agent => agent.agentId === editAgentId);
+
+        newAgents[newAgentIndex] = newAgent;
+
+        setAgents(newAgents);
+
+        resetForm();
+
+        setCurrentView('List')
     }
 
-    const addAgentClickHandler = () => {setCurrentView('Add')}
+    const agentEditClickHandler = (agentId) => {
+        setEditAgentId(agentId);
+
+        const editAgent = agents.find(agent => agent.agentId === agentId);
+
+        setFirstName(editAgent.firstName);
+        setMiddleName(editAgent.middleName);
+        setLastName(editAgent.lastName);
+        setDob(editAgent.dob);
+        setHeightInInches(editAgent.heightInInches);
+
+        setCurrentView('Edit');
+    }
+
+    const agentDeleteClickHandler = (agentId) => {
+
+        setAgents(agents.filter(agent => agent.id !== agentId));
+
+    };
+
+    const addAgentClickHandler = () => { setCurrentView('Add') }
 
     const agentListClickHandler = () => { setCurrentView('List'); }
 
@@ -93,8 +132,8 @@ function FieldAgents() {
                     </header>
                     <nav>
                         <div>
-                            <button type="button" onclick={homeClickHandler} >Home</button> | <a href="#">Agencies</a> | <a href="#">Locations</a> |
-                            <button type="button" onclick={agentListClickHandler}>Agents</button> | <a href="#">Missions</a>
+                            <button type="button" onClick={homeClickHandler} >Home</button> | <a href="#">Agencies</a> | <a href="#">Locations</a> |
+                            <button type="button" onClick={agentListClickHandler}>Agents</button> | <a href="#">Missions</a>
                         </div>
                     </nav>
                     <main>
@@ -144,6 +183,68 @@ function FieldAgents() {
                 </div>
             ) : null}
 
+            {currentView === 'Edit' ? (
+                <div class="container">
+                    <header>
+                        <h1>Field Agent</h1>
+                        <div>
+                            Welcome username! | <a href="#">Logout</a> | <a href="login.html">Login</a> | <a
+                                href="register.html">Register</a>
+                        </div>
+                    </header>
+                    <nav>
+                        <div>
+                            <button type="button" onClick={homeClickHandler} >Home</button> | <a href="#">Agencies</a> | <a href="#">Locations</a> |
+                            <button type="button" onClick={agentListClickHandler}>Agents</button> | <a href="#">Missions</a>
+                        </div>
+                    </nav>
+                    <main>
+
+                        <h2>Edit Agent</h2>
+
+                        <form onSubmit={editAgentFormSubmitHandler}>
+
+                            <div className="field">
+                                <label for="firstName">First Name:</label>
+                                <input id="firstName" name="firstName" type="text" value={firstName} onChange={firstNameOnChangeHandler}></input>
+                            </div>
+
+                            <div className="field">
+                                <label for="middleName">Middle Name:</label>
+                                <input id="middleName" name="middleName" type="text" value={middleName} onChange={middleNameOnChangeHandler}></input>
+                            </div>
+
+                            <div className="field">
+                                <label for="lastName">Last Name:</label>
+                                <input id="lastName" name="lastName" type="text" value={lastName} onChange={lastNameOnChangeHandler}></input>
+                            </div>
+
+                            <div className="field">
+                                <label for="dob">DOB:</label>
+                                <input id="dob" name="dob" type="date" value={dob} onChange={dobOnChangeHandler}></input>
+                            </div>
+
+                            <div className="field">
+                                <label for="heightInInches">Height in Inches:</label>
+                                <input id="heightInInches" name="heightInInches" type="number" min="0" max="200" value={heightInInches} onChange={heightInInchesOnChangeHandler}></input>
+                            </div>
+
+                            <button type="submit">Update Agent</button>
+
+                        </form>
+
+                        <a href="agents.html">Cancel</a>
+
+                    </main>
+                    <footer>
+                        <section>
+                            <h2>Legal Notices</h2>
+                            <p>Copyright &copy; 2021 Field Agent Inc. All rights reserved | <a href="#">Privacy Policy</a></p>
+                        </section>
+                    </footer>
+                </div>
+            ) : null}
+
             {currentView === 'Home' ? (
                 <>
                     <div class="container">
@@ -156,8 +257,8 @@ function FieldAgents() {
                         </header>
                         <nav>
                             <div>
-                                <button type="button" onclick={homeClickHandler} >Home</button> | <a href="#">Agencies</a> | <a href="#">Locations</a> |
-                                <button type="button" onclick={agentListClickHandler}>Agents</button> | <a href="#">Missions</a>
+                                <button type="button" onClick={homeClickHandler} >Home</button> | <a href="#">Agencies</a> | <a href="#">Locations</a> |
+                                <button type="button" onClick={agentListClickHandler}>Agents</button> | <a href="#">Missions</a>
                             </div>
                         </nav>
                         <main>
@@ -212,12 +313,14 @@ function FieldAgents() {
                             </div>
                         </header>
                         <nav>
-                            <a href='#' onclick={homeClickHandler}>Home</a> | <a href="#">Agencies</a> | <a href="#">Locations</a> | <a
-                                href='#' onclick={agentListClickHandler}>Agents</a> | <a href="#">Missions</a>
+                        <div>
+                            <button type="button" onClick={homeClickHandler} >Home</button> | <a href="#">Agencies</a> | <a href="#">Locations</a> |
+                            <button type="button" onClick={agentListClickHandler}>Agents</button> | <a href="#">Missions</a>
+                        </div>
                         </nav>
                         <main>
                             <h2>Agents</h2>
-                            <button type="button" onclick={addAgentClickHandler} >Add Agent</button>
+                            <button type="button" onClick={addAgentClickHandler} >Add Agent</button>
                             <div className="cards">
                                 {agents.map(agent => (
                                     <div className="card">
@@ -230,9 +333,9 @@ function FieldAgents() {
                                         </ul>
                                         <footer>
                                             <div>
-                                                <a href="edit-agent.html"><i class="bi bi-pencil-fill"></i> Edit</a>
+                                                <button onClick={() => agentEditClickHandler(agent.agentId)}><i class="bi bi-pencil-fill"></i> Edit</button>
                                                 &nbsp;|&nbsp;
-                                                <a href="delete-agent.html"><i class="bi bi-trash-fill"></i> Delete</a>
+                                                <button onClick={() => agentDeleteClickHandler(agent.agentId)}><i class="bi bi-trash-fill"></i> Delete</button>
                                             </div>
                                         </footer>
                                     </div>
